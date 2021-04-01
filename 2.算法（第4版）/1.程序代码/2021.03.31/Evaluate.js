56402758a00e32f8ea03fe3191fb7fff1fd3e099
wudong <wudong@uinnova.com>
2021-04-02 00:18:43 +0800

    feat: 没有困难的工作，只有勇敢的打工人🚀🚀🚀🚀✔

--- 2.算法（第4版）/1.程序代码/2021.03.31/Evaluate.js
+++ 2.算法（第4版）/1.程序代码/2021.03.31/Evaluate.js
@@ -1,40 +1,40 @@
-function Evaluate(formula){
+function Evaluate(formula) {
     const ops = [];
     const vals = [];
 
-    if(formula.indexOf('sqrt') !== -1){
-        formula = formula.replace('sqrt','#');
+    if (formula.indexOf('sqrt') !== -1) {
+        formula = formula.replace('sqrt', '#');
     }
     console.log(formula);
     formula.split('').forEach(s => {
-        if(s === '(') {
+        if (s === '(') {
 
-        }else if(s === '+'){
+        } else if (s === '+') {
             ops.push(s);
-        }else if(s === '-'){
+        } else if (s === '-') {
             ops.push(s);
-        }else if(s === '*'){
+        } else if (s === '*') {
             ops.push(s);
-        }else if(s === '/'){
+        } else if (s === '/') {
             ops.push(s)
-        }else if(s === '#'){
+        } else if (s === '#') {
             ops.push(s)
-        }else if(s === ')'){
+        } else if (s === ')') {
             let op = ops.pop();
             let v = vals.pop();
-            if(op === '+'){
-                v = vals.pop()+v;
-            }else if(op === '-'){
-                v = vals.pop()-v;
-            }else if(op === '*'){
-                v = vals.pop()*v;
-            }else if(op === '/'){
-                v = vals.pop()/v;
-            }else if(op === '#'){
+            if (op === '+') {
+                v = vals.pop() + v;
+            } else if (op === '-') {
+                v = vals.pop() - v;
+            } else if (op === '*') {
+                v = vals.pop() * v;
+            } else if (op === '/') {
+                v = vals.pop() / v;
+            } else if (op === '#') {
                 v = Math.sqrt(v);
             }
             vals.push(parseFloat(v));
-        }else{
+        } else {
             vals.push(parseFloat(s));
         }
     })
